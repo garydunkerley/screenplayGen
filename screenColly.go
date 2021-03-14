6629b5c69d206c87567fba877371d20c74899343
gary <garydunkerley@brandeis.edu>
2021-03-14 17:29:09 -0500

    In progress

--- screenColly.go
+++ screenColly.go
@@ -9,17 +9,17 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+		colly.AllowedDomains("imsdb.com/TV/Futurama"),
 	)
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		
+		if strings.HasPrefix(link, "/TV Transcripts/Futurama") || strings.HasPrefix(link, /trasncripts/Futurama) {
+			e.Request.Visit(link)
+		}
+
 	})
 
 	// Before making a request print "Visiting ..."
@@ -28,5 +28,5 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("imsdb.com/TV/Futurama.html")
 }
